b4c802792f89935493fed337786adac87f2552bd
CuiFg <1174910857@qq.com>
2016-06-01 09:39:13 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,16 +1,17 @@
 'use strict';
 
-function double_to_one(collection) {
+function double_to_one(collection)
+{
 
   var arr=[1, [2], [3, 4],[5,6,7,8]];
-var  newarr[];
-for(var i=0; i<arr.length; i++)
-{
-	for(var j=0; j< arr[i].length; j++)
-    {
-    	newarr.push(newarr[i][j]);
-    }
-}
+  var  newarr[];
+  for(var i=0; i<arr.length; i++)
+  {
+	    for(var j=0; j< arr[i].length; j++)
+      {
+        	newarr.push(newarr[i][j]);
+      }
+  }
 return newarr;
 }
 
