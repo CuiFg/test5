2460419e9860bb563c483ac351637f2203a970dd
CuiFg <1174910857@qq.com>
2016-06-02 21:20:48 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -4,15 +4,24 @@ function double_to_one(collection)
 {
 
  // var arr=[1, [2], [3, 4],[5,6,7,8]];
-  var  newarr=[];
-  for(var i=0; i<collection.length; i++)
-  {
-	    for(var j=0; j< collection[i].length; j++)
-      {
-        	newarr.push(collection[i][j]);
-      }
-  }
-return newarr;
+ var newarr=[];
+	for(var i=0; i<arr.length; i++)
+	{
+		if(arr[i].length ==undefined)
+		{
+			newarr.push(arr[i]);
+		}
+		else 
+		{
+			for(var j=0; j< arr[i].length; j++)
+			{
+				newarr.push(arr[i][j]); //将二维数组变成一维数组
+			}
+			
+		}
+	}
+//	document.write(arr+"<br>");
+	return newarr;
 }
 
 module.exports = double_to_one;
